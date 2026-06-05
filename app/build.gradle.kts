435da75d7bca5d8c46d7df4d341d42306e3d1a3e
maywine <maplewine@outlook.com>
2026-06-05 23:35:53 +0800

    chore: 版本号 0.1.7（versionCode 8）

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.tvbili"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.1.5"
+        versionCode = 8
+        versionName = "0.1.7"
 
         vectorDrawables { useSupportLibrary = true }
 
